e5d2eba3d66a7ed21979623bf3ccaa99d686c550
Guillem257 <guillem2525@gmail.com>
2024-10-18 11:05:06 +0200

    grm_pausa

--- Arkanoid_GMR/Assets/Scripts/GameManager.cs
+++ Arkanoid_GMR/Assets/Scripts/GameManager.cs
@@ -3,23 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class GameManager : MonoBehaviour
 {
-
-
     public int lives = 3;
+    private bool isPaused = false; // Variable para verificar si el juego está en pausa
 
+    public GameObject pauseUI;
+
+    void Update()
+    {
+        // Si se presiona la tecla Escape, se alterna el estado de pausa
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TogglePause();
+        }
+    }
+
+    // Método para alternar la pausa del juego
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    // Pausar el juego
+    public void PauseGame()
+    {
+        Time.timeScale = 0f; // Detiene el tiempo del juego
+        isPaused = true;
+        // Aquí podrías mostrar el menú de pausa si tienes uno
+        pauseUI.SetActive(true);
+    }
+
+    // Reanudar el juego
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f; // Restablece el tiempo del juego
+        isPaused = false;
+        pauseUI.SetActive(false);
+        // Aquí podrías ocultar el menú de pausa si tienes uno
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        pauseUI.SetActive(false);
+        SceneManager.LoadScene("GameMenu");
+
+    }
 
     public void LosseHealth()
     {
-
         lives--;
 
-        if (lives <=0)
+        if (lives <= 0)
         {
             SceneManager.LoadScene("GameOver");
-
         }
         else
         {
@@ -29,15 +75,13 @@ public class GameManager : MonoBehaviour
 
     public void ResetLevel()
     {
-
         FindObjectOfType<Ball>().ResetBall();
         FindObjectOfType<Player>().ResetPlayer();
     }
 
     public void CheckLevelComplet()
     {
-
-        if (transform.childCount <=1)
+        if (transform.childCount <= 1)
         {
             Scene escenaActual = SceneManager.GetActiveScene();
 
@@ -49,8 +93,6 @@ public class GameManager : MonoBehaviour
             {
                 SceneManager.LoadScene("Level1");
             }
-
         }
-        
     }
 }
